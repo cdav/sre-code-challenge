b0b2a56e478a868841d4d94972518d2b18a60b89
Cory Davenport <coryrdavenport@gmail.com>
2019-10-30 13:45:48 -0400

    flask Config vars loaded from env

--- flask/config.py
+++ flask/config.py
@@ -1,5 +1,7 @@
+import os
+
 class Config(object):
-    MYSQL_DATABASE_USER = 'MYUSER'
-    MYSQL_DATABASE_PASSWORD = 'MYPASSWORD'
-    MYSQL_DATABASE_DB = 'mydb'
-    MYSQL_DATABASE_HOST = 'scc-mysql'
+    MYSQL_DATABASE_USER = os.environ.get('MYSQL_DATABASE_USER')
+    MYSQL_DATABASE_PASSWORD = os.environ.get('MYSQL_DATABASE_PASSWORD')
+    MYSQL_DATABASE_DB = os.environ.get('MYSQL_DATABASE_DB')
+    MYSQL_DATABASE_HOST = os.environ.get('MYSQL_DATABASE_HOST')
